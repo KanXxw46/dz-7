477d34509e628aa78ba1477763f1409ee70241ff
KanXxw46 <1kan_andreikkkk@mail.ru>
2020-06-25 20:45:31 +0600

    HomeWork

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using HOmeWirk7;
+
+namespace Dz_7HomeWork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person andrew = new Person { name = "Andrew", age = 16 };
+            Console.WriteLine(andrew.name);
+        }
+    }
+}
